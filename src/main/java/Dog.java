7a128b1b0ea767d34a125e37ca4eb23efb47b1d3
lucass <24lspeier@athenian.org>
2021-11-01 13:20:28 -0700

    Tesseract

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,61 @@
+import org.w3c.dom.NameList;
+
 public class Dog extends Pet {
     // Instance variable(s)
-
-
+    String breed;
+    String name;
+    int age;
     // Constructors
-    
-
+    public Dog () {
+        super();
+        breed = "Bulldog";
+        name = getName();
+        age = getAge();
+    }
+    public Dog (String Name, int Age, String Breed) {
+        super(Name,Age);
+        name = Name;
+        age = Age;
+        breed = Breed;
+    }
     // makeNoise() method
-
+    public void makeNoise() {
+        System.out.println("Bark");
+    }
 
     // toString method
-
+    public String toString() {
+        return "Name: " + name + ", Age: " + age + ", Breed: " + breed;
+    }
 
     // Getter
 
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+    public String getBreed() {
+        return breed;
+    }
 
     // Setter
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,16 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog myDog = new Dog("Samuel",9461, "German Shepherd");
+        System.out.println(myDog.toString());// Name: Samuel, Age: 9461, Breed: German Shepherd
+        System.out.println(myDog.getAge());//9461
+        System.out.println(myDog.getName());//Samuel
+        System.out.println(myDog.getBreed());//German Shepherd
+        myDog.setBreed("Poodle");
+        myDog.setAge(12);
+        myDog.setName("Jerry");
+        myDog.makeNoise(); // Bark
+        System.out.println(myDog.getAge()); // 12
+        System.out.println(myDog.getName()); // Jerry
+        System.out.println(myDog.getBreed()); // Poodle
     }
 }
